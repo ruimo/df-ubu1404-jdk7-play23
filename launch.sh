777932b08be30c6d461eda049f28a17f6781663a
Shisei Hanai <ruimo.uno@gmail.com>
2014-07-11 16:47:40 +0900

    Fix launch.sh

--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -e "HOST_UID=`id -u`" -e "HOST_USER=$USER" -p 9001:9000 -v $HOME:/var/home:rw -i -t ruimo/dockerfile-ubuntu1404-java8-pl $*
+docker run -e "HOST_UID=`id -u`" -e "HOST_USER=$USER" -p 9001:9000 -v $HOME:/var/home:rw -i -t ruimo/df-ubu1404-jdk7-play23 $*
