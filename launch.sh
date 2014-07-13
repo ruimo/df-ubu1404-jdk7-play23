bc8592ba291cbb239041c3c585d6103d9583e56c
Shisei Hanai <ruimo.uno@gmail.com>
2014-07-13 19:02:29 +0900

    Not use /var/home since IDE will be confused.

--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -e "HOST_UID=`id -u`" -e "HOST_USER=$USER" -p 9001:9000 -v $HOME:/var/home:rw -i -t ruimo/df-ubu1404-jdk7-play23 $*
+docker run -e "HOST_UID=`id -u`" -e "HOST_USER=$USER" -p 9001:9000 -v $HOME:$HOME:rw -i -t $* ruimo/df-ubu1404-jdk7-play23
--- profile
+++ profile
@@ -1,2 +1,2 @@
-useradd -d /var/home -s /bin/bash --user-group -u $HOST_UID $HOST_USER
+useradd -d /home/$HOST_USER -s /bin/bash --user-group -u $HOST_UID $HOST_USER
 su - $HOST_USER
